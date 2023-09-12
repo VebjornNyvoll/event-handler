21b51e042dd08946b57e16dd85aaa5ce05be0722
Vebjørn Nyvoll <vebjorn.nyvoll@gmail.com>
2023-09-12 10:53:07 +0200

    Applied prettier to .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,20 @@
 /* eslint-env node */
 
-
-
 module.exports = {
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended-type-checked', 'plugin:react/recommended', 'plugin:react/jsx-runtime'],
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
-    root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
 
-    parserOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      project: ['./tsconfig.json', './tsconfig.node.json'],
-      tsconfigRootDir: __dirname,
-     },
-  };
-
-  
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
+};
