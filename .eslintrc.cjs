252eb56bc824cab693d2ebb0b553e7f10ae03089
Vebjørn Nyvoll <vebjorn.nyvoll@gmail.com>
2023-09-12 10:38:12 +0200

    Added .eslintrc.cjs with recommended rules

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+/* eslint-env node */
+
+
+
+module.exports = {
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended-type-checked', 'plugin:react/recommended', 'plugin:react/jsx-runtime'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    root: true,
+
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      project: ['./tsconfig.json', './tsconfig.node.json'],
+      tsconfigRootDir: __dirname,
+     },
+  };
+
+  
