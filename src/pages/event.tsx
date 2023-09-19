9ce00d9d0da081300b7aeda5176ec28460b5ccc3
Vebjørn Nyvoll <vebjorn.nyvoll@gmail.com>
2023-09-19 17:56:59 +0200

    Added get-event-data.ts and a call for it in event.tsx

--- src/pages/event.tsx
+++ src/pages/event.tsx
@@ -1,10 +1,13 @@
 import { FC } from 'react';
 import { useParams } from 'react-router-dom';
+import { getEventData } from '../queries/get-event-data.ts';
 
 type Params = { eventId: string };
 
 export const EventPage: FC = () => {
   const params = useParams<Params>();
+  const eventDetails = getEventData(params.eventId);
+  
 
-  return <h1>Hello world, this is event {params.eventId}</h1>;
+  return <h1>Hello world, this is event {eventDetails.name}</h1>;
 };
