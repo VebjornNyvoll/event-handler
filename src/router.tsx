bedb1adebfe2e7a46e87c1b27b519b9685dfa469
Mats Larsen <mats@jun.codes>
2023-09-20 22:32:29 +0200

    Add favorites page

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 import { HomePage } from './pages/home.tsx';
 import { EventPage } from './pages/event.tsx';
+import { Favorites } from './pages/favorites.tsx';
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <>
+      <Route path="/favorites" element={<Favorites />} />
       <Route path="/" element={<HomePage />}>
         <Route path=":eventId" element={<EventPage />} />
       </Route>
